4ef416fd111cf083247a6f85012ea3eb5ccaccd2
Andrea Fàbregas Martínez <a.fabregas@outlook.es>
2022-07-05 12:57:30 +0200

    Search song form, route and view implemented.

--- routes/app/search.routes.js
+++ routes/app/search.routes.js
@@ -5,13 +5,11 @@ const isLoggedOut = require("../../middleware/isLoggedOut");
 const isLoggedIn = require("../../middleware/isLoggedIn");
 const SpotifyWebApi = require("spotify-web-api-node");
 
-// setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
 
-// Retrieve an access token
 spotifyApi
   .clientCredentialsGrant()
   .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
@@ -19,46 +17,61 @@ spotifyApi
     console.log("Something went wrong when retrieving an access token", error)
   );
 
+// ROUTES
+
 router.get("/", (req, res, next) => {
   res.render("search/form.hbs");
 });
 
-router.get("/artist", (req, res, next) => {
-  const { artist } = req.query;
-  spotifyApi
-    .searchArtists(artist)
-    .then((data) => {
-      res.render("search/artists.hbs", {
-        artist: data.body.artists.items,
+router.post("/results", (req, res, next) => {
+  const { value, type } = req.body;
+  if (type === "song") {
+    spotifyApi
+      .searchTracks(value)
+      .then((track) => {
+        res.render("search/tracks.hbs", { track: track.body.tracks.items });
+        console.log(
+          "AQUÍ EMPIEZAAAAAAA 👍👍👍👍👍👍👍",
+          track.body.tracks.items[0].artists[0].name
+        );
+      })
+      .catch((error) => {
+        res.render("search/form.hbs", { errorMessage: error.message });
       });
-    })
-    .catch((err) =>
-      console.log("The error while searching artists occurred: ", err)
-    );
+  }
+  //     .searchArtists(artist)
+  //     .then((data) => {
+  //       res.render("search/artists.hbs", {
+  //         artist: data.body.artists.items,
+  //       });
+  //     })
+  //     .catch((err) =>
+  //       console.log("The error while searching artists occurred: ", err)
+  //     );
 });
 
-router.get("albums/:artistId", (req, res, next) => {
-  const { artistId } = req.params;
-  spotifyApi
-    .getArtistAlbums(artistId)
-    .then((data) => {
-      res.render("albums.hbs", { album: data.body.items });
-    })
-    .catch((err) =>
-      console.log(artistId, "The error while searching albums occurred: ", err)
-    );
-});
+// router.get("albums/:artistId", (req, res, next) => {
+//   const { artistId } = req.params;
+//   spotifyApi
+//     .getArtistAlbums(artistId)
+//     .then((data) => {
+//       res.render("albums.hbs", { album: data.body.items });
+//     })
+//     .catch((err) =>
+//       console.log(artistId, "The error while searching albums occurred: ", err)
+//     );
+// });
 
-router.get("tracks/:albumId", (req, res, next) => {
-  const { albumId } = req.params;
-  spotifyApi
-    .getAlbumTracks(albumId)
-    .then((data) => {
-      res.render("search/tracks.hbs", { track: data.body.items });
-    })
-    .catch((err) =>
-      console.log("The error while searching albums occurred: ", err)
-    );
-});
+// router.get("tracks/:albumId", (req, res, next) => {
+//   const { albumId } = req.params;
+//   spotifyApi
+//     .getAlbumTracks(albumId)
+//     .then((data) => {
+//       res.render("search/tracks.hbs", { track: data.body.items });
+//     })
+//     .catch((err) =>
+//       console.log("The error while searching albums occurred: ", err)
+//     );
+// });
 
 module.exports = router;
