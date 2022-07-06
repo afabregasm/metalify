1b33833c0b9c2429a79ad96c8f8445895d144b0d
fRanaRfs <105783080+fRanaRfs@users.noreply.github.com>
2022-07-06 13:14:55 +0200

    Pull
    Delete echo, update en proceso

--- routes/app/playlist.routes.js
+++ routes/app/playlist.routes.js
@@ -2,12 +2,85 @@ const router = require("express").Router();
 const Playlist = require("../../models/Playlist.model");
 const User = require("../../models/User.model");
 
-router.get('/new-playlist',(req, res, next) => {
-  res.render('user/new-playlist.hbs', {user: {playlistname: ''}})
-})
+router.get("/new-playlist", (req, res, next) => {
+  res.render("user/new-playlist.hbs", { user: { playlistname: "" } });
+});
 
+router.get("/new-playlist", (req, res, next) => {
+  Playlist.create()
+    .then(() => {
+      res.render("user/my-playlist.hbs");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-router.post('/new-playlist', (req, res, next) => {
+router.post("/new-playlist", (req, res, next) => {
+  const { newplaylist } = req.body;
+  console.log(newplaylist);
+  Playlist.create({
+    playlistname: newplaylist,
+  })
+    .then((user) => {
+      res.render("user/profile.hbs", { user: { playlistname: newplaylist } });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/my-playlist", (req, res, next) => {
+  Playlist.find()
+    .then((response) => {
+      res.render("user/my-playlist.hbs", { response });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/my-playlist", (req, res, next) => {
+  res.render("user/costume-playlist", { response });
+});
+
+router.get("/costume-playlist/:id", (req, res, next) => {
+  const { id } = req.params;
+  Playlist.findById(id)
+  .then((playlist) => {
+    res.render("user/costume-playlist", { playlist });
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.post("/:id/delete", (req, res, next) => {
+  console.log(req.params.id)
+  Playlist.findByIdAndRemove(req.params.id)
+  .then((response) => {
+    console.log(response)
+    res.redirect('/')
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+router.post("/:id/update", (req, res, next) => {
+  console.log(req.params.id)
+  const {playlistname} = req.params
+  Playlist.findByIdAndUpdate(req.params.id, {playlistname})
+  .then((response) => {
+    console.log(response)
+    res.redirect('user/update-form')
+  })
+  .catch((err) => {
+    next(err);
+  });
+});
+
+/*router.post('/new-playlist', (req, res, next) => {
   const {newplaylist} = req.body 
    console.log(newplaylist)
   Playlist.create({
@@ -24,15 +97,14 @@ router.post('/new-playlist', (req, res, next) => {
 
     router.get('/my-playlist',(req, res, next) => {
       res.render('user/my-playlist.hbs')
-    });
+    }); */
 
-  /*  Playlist.create()
+/*  Playlist.create()
 .then((newplaylist) => {
   User.findByIdAndUpdate({$push: {newplaylist},
   }) .then(() => {
     res.redirect("/profile")
   })
 }); */
-
 
 module.exports = router;
